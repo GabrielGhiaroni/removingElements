0b94c07a2f9936b63507d172816cc741f5c74f84
Gabriel Ghiaroni <88748657+GabrielGhiaroni@users.noreply.github.com>
2023-10-17 23:10:59 -0300

    Update solution.cs

--- solution.cs
+++ solution.cs
@@ -1,1 +1,19 @@
+  public static class Kata
+    {
+        public static object[] RemoveEveryOther(object[] arr)
+        {
+          int tamanhoNovoArray = (arr.Length + 1) / 2;
+          
+          object[] novoArray = new object[tamanhoNovoArray];
 
+          int novoIndex = 0;
+
+          for (int i = 0; i < arr.Length; i += 2)
+          {
+              novoArray[novoIndex] = arr[i];
+              novoIndex++;
+          }
+
+          return novoArray;
+        }
+    }
